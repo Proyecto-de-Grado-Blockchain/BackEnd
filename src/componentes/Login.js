a8e65a13fd5db3e10c05afeb1521af73aa9154a5
Santiago-Florez <florezsantiago50@gmail.com>
2024-09-21 14:18:28 -0500

    login terminado

--- src/componentes/Login.js
+++ src/componentes/Login.js
@@ -1,34 +1,57 @@
-import React, { useState } from 'react';
-import Imagendoc from '../imagenes/Imagendoc.png';
-import logo from '../imagenes/logo.png';
-import ojoA from '../imagenes/ojoA.png';
-import ojoC from '../imagenes/ojoC.png';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import Imagendoc from "../imagenes/Imagendoc.png";
+import logo from "../imagenes/logo.png";
+import ojoA from "../imagenes/ojoA.png";
+import ojoC from "../imagenes/ojoC.png";
+import { useNavigate } from "react-router-dom";
 
 export const Login = () => {
   const [showPassword, setShowPassword] = useState(false);
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
   const navigate = useNavigate();
 
   const PasswordVisibility = () => {
     setShowPassword(!showPassword);
   };
 
-  const handleSubmit = (event) => {
-    event.preventDefault(); 
-    if (email === 'admin@a' && password === 'admin') {
-      sessionStorage.setItem('authenticated', 'true');
-      navigate('/home'); 
-    } else {
-      setError('Correo o Contraseña incorrectos');
-
-      setTimeout(() => {
-        setError('');
-      }, 2000);
+  async function useHandleSubmit(event) {
+    event.preventDefault();
+    try {
+      await fetch(`http://localhost:3100/usuarios/login`, {
+        method: "POST", // Utilizamos el método POST
+        headers: {
+          "Content-Type": "application/json", // Indicamos que estamos enviando JSON
+        },
+        body: JSON.stringify({
+          // Convertimos el objeto a JSON
+          correoReq: email,
+          contrasenaReq: password,
+        }),
+      }).then(response => {
+        // Convertimos el objeto Response a JSON
+        if (!response.ok) {
+          throw new Error('Error en la respuesta del servidor');
+        }
+        return response.json(); // Devuelve una promesa que se resuelve a JSON
+      })
+      .then((data) => {
+        console.log("DATA: " + data);
+        if (data != null ) {
+          sessionStorage.setItem("authenticated", "true");
+          navigate("/home");
+        } else {
+          setError("Correo o Contraseña incorrectos");
+          setTimeout(() => {
+            setError("");
+          }, 2000);
+        }
+      });
+    } catch (error) {
+      console.error("Error en la validación de inicio de sesión:", error);
     }
-  };
+  }
 
   return (
     <div className="login-container">
@@ -48,7 +71,7 @@ export const Login = () => {
           <div className="form-container">
             <h2>Bienvenido</h2>
             {error && <div className="error-message">{error}</div>} {}
-            <form onSubmit={handleSubmit}>
+            <form onSubmit={useHandleSubmit}>
               <div className="input-correo">
                 <input
                   type="email"
@@ -78,8 +101,10 @@ export const Login = () => {
                     />
                   </button>
                 </div>
-              </div>  
-              <button className="submit" type="submit">Iniciar Sesión</button>
+              </div>
+              <button className="submit" type="submit">
+                Iniciar Sesión
+              </button>
             </form>
             <div className="image-container">
               <img src={Imagendoc} alt="Imagen del documento" />
