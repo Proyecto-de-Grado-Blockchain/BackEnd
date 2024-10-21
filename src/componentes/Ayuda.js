0c4dfe5ad3956a82d58b43583184f1fd6e898cdf
yilberR <yilrojas@unbosque.edu.co>
2024-10-20 22:46:26 -0500

    a

--- src/componentes/Ayuda.js
+++ src/componentes/Ayuda.js
@@ -16,33 +16,13 @@ export const Ayuda = () => {
       <div className="casos-detalle-container">
         <h2>Centro de Ayuda y Soporte</h2>
         <p>
-          En el Centro de Ayuda y Soporte, puedes encontrar respuestas a tus
-          preguntas, acceder a recursos Utiles y contactar con nuestro equipo de
-          soporte para obtener asistencia personalizada. Explora nuestras guías,
-          tutoriales y preguntas frecuentes para resolver cualquier duda o
-          problema que puedas tener.
+        Aquí encontrarás el manual de la applicación, específico para cada usuario o rol.
         </p>
       </div>
       <br />
       <br />
 
       <div className="DocumentosExistentes">
-        {/*
-        <div className="documento">
-          <img src={manual} alt="Gestión de Casos Activos" className="module-image" />
-          <h4 className="module-title">Manual Técnico</h4>
-          <p>Guía detallada sobre la configuración,  
-            mantenimiento y funcionamiento técnico del sistema para administradores y desarrolladores.</p>
-          <div className="button-subir">
-            <Link to="/subir-doc">
-              <button className="btn btn-secondary module-btn">
-                <img src={ojoA} alt="Mirar Documento" className="white-image" />
-              </button>
-            </Link>
-          </div>
-        </div>
-        */}
-
         <div className="documento">
           <img
             src={manual}
@@ -51,7 +31,9 @@ export const Ayuda = () => {
           />
           <h4 className="module-title">Manual de Usuario</h4>
           <p>
-            Aquí encontrarás el manual de la applicación, específico para cada usuario o rol.
+              Instrucciones paso a paso para que los usuarios finales puedan
+              utilizar las funciones y características del sistema de manera
+              eficiente.
           </p>
           <iframe
             src={documentUrl}
---
+++ src/componentes/Visualizar.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Visualizar = () => {
+  return (
+    <div>Visualizar</div>
+  )
+}
